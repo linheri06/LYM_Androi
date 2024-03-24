cefd339968075ead59e104a4149fd922b2adeb78
linheri06 <100428580+linheri06@users.noreply.github.com>
2024-03-24 18:47:06 +0700

    giao dien

---
+++ app/src/main/java/com/example/lym/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.lym;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(MainActivity.this, TakePhotoActivity.class);
+        startActivity(intent);
+    }
+}
